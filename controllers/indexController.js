31796c7ef478d71ddb9d7fe32eb27d49fe588077
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-03 21:44:44 +0300

    modify index controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,26 +5,23 @@ const { PrismaClient }  = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const registerUser = async (req, res) => {
-    console.log(req.body);
-    
     const users = await prisma.users.findMany();
     const exists = users.find(user => user.username === req.body.username);
     if (exists) {
         return res.sendStatus(404)
     }
-    // create user
     else {
         await User.createUser(req.body)
         return res.sendStatus(200)
     }
 }
 
+
 const registerAdmin = async (req, res) => {
-    console.log(req.body);
     const users = await prisma.users.findMany();
     const exists = users.find(user => user.username === req.body.username);
     if (exists) {
-        return res.end("user exists and go back")
+        return res.status(404).json({error: "user exists and go back"});
     }
     if (req.body.admin_pwd && req.body.admin_pwd === process.env.ADMIN_PASSWORD){
         await User.createAdmin(req.body);
@@ -34,20 +31,18 @@ const registerAdmin = async (req, res) => {
     }
 }
 
-async function getAllsuert(id) {
-//     const user = await prisma.users.findFirst({where: {users_id: id}})
-//     const comment = await prisma.comment.deleteMany({where: {user_id: id}})
-//     const post = await prisma.post.deleteMany({where: {user_id: id}})
-//     await prisma.users.delete({
-//         where : {
-//             users_id: id
-//         }
-//     })
-    console.log(await prisma.users.findMany())
+
+const allUsersGet = async (req, res) => {
+    if (req.user.Role === "ADMIN") {
+        const users = await User.fetchAllUsers();
+        return res.json({users})
+    } else {
+        res.sendStatus(401)
+    }
 }
 
-getAllsuert("b83925ac-b9c3-4c43-9538-a3a7e2f34be8")
 module.exports = {
     registerUser,
-    registerAdmin
+    registerAdmin,
+    allUsersGet
 }
