4c2d32523724583b25bced5771a315769e7e9146
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-02-25 19:58:47 +0300

    3-refactor-endpoints fix rate limiting

--- middlewares/rateLimitter.js
+++ middlewares/rateLimitter.js
@@ -4,7 +4,7 @@ const {rateLimit} = require("express-rate-limit")
 const limitAccess = rateLimit({
     keyGenerator: (req, res) => req.user.id || req.ip,
     max: 10,
-    windowMs: 10 * 60 * 1000,
+    windowMs: 2 * 60 * 1000,
     handler: (req, res) => res.status(429).json({ success: false, message: "Too many requests!" }),
     skip: (req, res) => req.user.Role === "ADMIN"
 })
